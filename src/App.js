6515576a85c76dab5798c5c161f0cc61abd57aa9
9mean2 <o3omoomin@gmail.com>
2023-02-06 01:22:55 +0900

    feat 반복되는 컴포넌트 처리 추가버튼

--- src/App.js
+++ src/App.js
@@ -1,20 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
-const App = () => {
-  const testArr = ["감자", "고구마", "오이", "가지", "옥수수"];
+function App() {
+  const [users, setUsers] = useState([
+    { id: 1, age: 30, name: "송중기" },
+    { id: 2, age: 24, name: "송강" },
+    { id: 3, age: 21, name: "김유정" },
+    { id: 4, age: 29, name: "구교환" },
+  ]);
+
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+
+  const nameChangeHandler = (event) => {
+    setName(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setAge(event.target.value);
+  };
+
+  const buttonClickHandler = () => {
+    const newUser = {
+      id: users.length + 1,
+      age: age,
+      name: name,
+    };
+
+    setUsers([...users, newUser]);
+  };
 
   return (
-    <div className="app-style">
-      {testArr
-        .filter(function (item) {
-          return item !== "오이";
-        })
-        .map(function (item) {
-          return <div className="component-style">{item}</div>;
+    <div>
+      <div>
+        이름 : <input value={name} onChange={nameChangeHandler} /> <br />
+        나이 : <input value={age} onChange={ageChangeHandler} /> <br />
+        <br />
+        <button onClick={buttonClickHandler}>추가</button>
+      </div>
+      <div className="app-style">
+        {users.map(function (item) {
+          return (
+            <div key={item.id} className="component-style">
+              {item.age} - {item.name}{" "}
+            </div>
+          );
         })}
+      </div>
     </div>
   );
-};
+}
 
 export default App;
